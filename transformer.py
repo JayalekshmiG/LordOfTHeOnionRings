85d27b69817024267081ea2e5106f15f67078bbe
jayalekshmigopakumar <lekshmi.ntg@gmail.com>
2023-04-01 12:46:10 +0100

    files

---
+++ transformer.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a0408bc0577de9d7911b7290f712868633063da4974cb4647e3707b2ea6668e7
+size 13989
---
+++ translate_movies.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:760504cc2614aaf2b6f2b0a4326fb8f275790306c160761789eb60e1e5751099
+size 12609
